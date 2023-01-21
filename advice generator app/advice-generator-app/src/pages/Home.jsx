949992a1625361811eb0c0b5f2eea235ab0e0d01
archit nidhi <118736421+architnidhi@users.noreply.github.com>
2023-01-21 12:22:09 +0530

    Add files via upload

---
+++ advice generator app/advice-generator-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+import CardDiv from '../components/card/CardDiv';
+
+function Home() {
+    return (
+        <div className='container'>
+            <CardDiv />
+        </div>
+    )   
+}
+
+export default Home;
